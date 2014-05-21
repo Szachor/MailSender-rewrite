30c4bead5a7fb4f5b83de21fae3d3dc73e48501b
Szachor <chrystian.kislo@gmail.com>
2014-05-21 13:59:06 +0200

    Testy zaczęte :p

---
+++ UnitTests/UnitTestsMandrill.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MailSender;
+using MandrillMailSender;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestsMandrill
+    {
+        static Mail mail;
+        static MandrillSender sender;
+
+        [ClassInitialize]
+        static public void initializeClass(TestContext context)
+        {
+            mail = new Mail("Test content", "test subject", false);
+            sender = new MandrillSender("api-key","from mail");
+        }
+        [TestMethod]
+        public void SendMailTestResponseSent()
+        {
+            Response response = sender.SendMail(mail);
+            Assert.AreEqual("", response.Message);
+        }
+        [TestMethod]
+        public void SendMailTestResponseWrongMail()
+        {
+
+        }
+        [TestMethod]
+        public void SendMailTestResonse()
+        {
+        }
+        [TestMethod]
+        public void SendMailTest()
+        {
+        }
+    }
+}
